a19eeb64d71bd25d10ef9df6a38163967a7f4e10
sussyamogus16 <amogussussus16@gmail.com>
2025-01-17 14:12:13 -0600

    Add files via upload

---
+++ moneyscript.lua
@@ -0,0 +1,25 @@
+local plr = game:GetService("Players").LocalPlayer
+local Notification = require(game:GetService("ReplicatedStorage").Notification)
+local Data = plr:WaitForChild("Data")
+local EXPFunction = require(game.ReplicatedStorage:WaitForChild("EXPFunction"))
+local LevelUp = require(game:GetService("ReplicatedStorage").Effect.Container.LevelUp)
+local Sound = require(game:GetService("ReplicatedStorage").Util.Sound)
+local LevelUpSound = game:GetService("ReplicatedStorage").Util.Sound.Storage.Other:FindFirstChild("LevelUp_Proxy") or game:GetService("ReplicatedStorage").Util.Sound.Storage.Other:FindFirstChild("LevelUp")
+function v129(p15)
+    local v130 = p15;
+    while true do
+        local v131, v132 = string.gsub(v130, "^(-?%d+)(%d%d%d)", "%1,%2");
+        v130 = v131;
+        if v132 == 0 then
+            break;
+        end;    
+    end;
+    return v130;
+end;
+
+Notification.new("<Color=Yellow>You are not broke now<Color=/>"):Display()
+Notification.new("Earned <Color=Green>$2,500,000<Color=/>"):Display()
+plr.Data.Beli.Value = plr.Data.Beli.Value + 2500000
+        wait()
+    end
+end
